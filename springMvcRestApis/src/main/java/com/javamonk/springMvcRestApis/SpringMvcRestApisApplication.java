97bba00812a0b732e16a0a8ac2fdc9461c343ebd
vishal saxena <saxena.vishal806@gmail.com>
2026-09-13 17:12:53 +0530

    initial commit

---
+++ springMvcRestApis/src/main/java/com/javamonk/springMvcRestApis/SpringMvcRestApisApplication.java
@@ -0,0 +1,13 @@
+package com.javamonk.springMvcRestApis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcRestApisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcRestApisApplication.class, args);
+	}
+
+}
---
+++ springMvcRestApis/src/test/java/com/javamonk/springMvcRestApis/SpringMvcRestApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javamonk.springMvcRestApis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcRestApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
